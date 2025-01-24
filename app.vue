172e73fd688eafbdd3c59851526fc11ba35ebff5
René Zschoch <r.zschoch@me.com>
2025-01-24 16:54:34 +0100

    Clean-up basic project configuration

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<HelloWorld />
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2025-01-24',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint'],
+	typescript: {
+		typeCheck: true,
+	},
+	extends: [
+		['madek-api-nuxt-layer'],
+		// ['github:orc-hfg/madek-api-nuxt-layer#1.0.0-alpha', { install: true }],
+		// 'github:username/repoName/base', // GitHub Remote Source within /base directory
+		// 'github:username/repoName#dev', // GitHub Remote Source from dev branch
+		// 'github:username/repoName#v1.0.0', // GitHub Remote Source from v1.0.0 tag
+	],
+});
