e96b6c0ff871acab4e9a53851b87f01d121e982d
René Zschoch <r.zschoch@me.com>
2025-03-06 14:57:16 +0100

    Use pinia store

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
 	<div>
-		{{ authInfo.first_name }}
+		{{ userStore.displayName }}
 	</div>
 </template>
 
-<script setup>
-const { data: authInfo, pending, error } = useFetch('/api/auth-info');
+<script setup lang="ts">
+const userStore = useUserStore();
+
+await callOnce(userStore.fetchData);
 </script>
