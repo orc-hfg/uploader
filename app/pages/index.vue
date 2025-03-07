ccf808a3a7eb2c8cc99d77b1b26cfc169a9f248c
René Zschoch <r.zschoch@me.com>
2025-03-07 18:30:09 +0100

    Add example for direct repository access

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
 	<div>
-		{{ userStore.displayName }}
+		<p>
+			User Store Getter (displayName):<br />
+			{{ userStore.displayName }}
+		</p>
+		<p>
+			User Repository (id):<br />
+			{{ data?.id }}
+		</p>
 	</div>
 </template>
 
 <script setup lang="ts">
 const userStore = useUserStore();
-
 await callOnce(userStore.fetchData);
+
+const userRepository = useUserRepository();
+const { data } = await useAsyncData(() => userRepository.getAuthInfo());
 </script>
