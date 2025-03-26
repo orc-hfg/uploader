b184ac1eb07cbec8aafe6da8aa3d392a8622e1f4
René Zschoch <r.zschoch@me.com>
2025-03-26 18:33:02 +0100

    Install pinia, update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,14 @@ export default defineNuxtConfig({
 	future: {
 		compatibilityVersion: 4,
 	},
-	compatibilityDate: '2025-01-24',
+	compatibilityDate: "2025-01-24",
 	devtools: { enabled: true },
-	modules: ['@nuxt/eslint'],
+	modules: ["@nuxt/eslint", "@pinia/nuxt"],
 	typescript: {
 		typeCheck: true,
 	},
 	runtimeConfig: {
 		isMainApp: true,
 	},
-	extends: ['@orc-hfg/madek-api-nuxt-layer'],
+	extends: ["@orc-hfg/madek-api-nuxt-layer"],
 });
