52fd62be67e05a4a3514e5d7711e2815181a351e
René Zschoch <r.zschoch@me.com>
2025-07-16 11:05:48 +0200

    Fix vitest command in lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
 	'*.{ts,vue,md,json}': 'eslint --fix',
-	'*.{ts,vue}': () => ['npm run check:types', 'npm run check:unused', 'vitest related --run'],
+	'*.{ts,vue}': () => ['npm run check:types', 'npm run check:unused', 'vitest run'],
 };
