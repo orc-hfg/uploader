4947540679d37975efcb58b5152852458fe6b536
René Zschoch <r.zschoch@me.com>
2025-07-18 22:17:50 +0200

    Remove e2e test from lint-staged hooks (performance)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
 	'*.{ts,vue,md,json,yml,yaml}': 'eslint --fix',
-	'*.{ts,vue}': () => ['npm run check:types', 'npm run check:unused', 'npm run test', 'npm run test:e2e'],
+	'*.{ts,vue}': () => ['npm run check:types', 'npm run check:unused', 'npm run test'],
 };
